fea4012980d1b829e6a07d69319ad624cd440025
freedox10 <alfre2636@yahoo.com.ar>
2024-03-19 18:11:00 -0300

    Agregada validación caracteres

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+//variables para DOM
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
+const informacion = document.querySelector(".informacion");
 const infomsg1 = document.querySelector(".informacion-msg1");
 const infomsg2 = document.querySelector(".informacion-msg2");
 const btncopiar = document.querySelector(".copiar");
+//llave de encriptacion
+const matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+//caracteres no válidos
+const excluidos = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù'ñÑA-Z]/g;
 
 // Muestra resultados encriptación
 function btnEncriptar(){
-	const textoEncriptado = encriptar(textArea.value);
-	mensaje.value = textoEncriptado;
-	textArea.value = "";
-	mensaje.style.backgroundImage = "none";
-	infomsg1.style.visibility = "hidden";
-	infomsg2.style.visibility = "hidden";
-	btncopiar.style.display = "block";
+	if (validar(textArea.value)) {
+		const textoEncriptado = encriptar(textArea.value);
+		mensaje.value = textoEncriptado;
+		textArea.value = "";
+		mensaje.style.backgroundImage = "none";
+		infomsg1.style.visibility = "hidden";
+		infomsg2.style.visibility = "hidden";
+		btncopiar.style.display = "block";
+	}
 }
 
 // Función encriptar
 function encriptar(stringEncriptada){
-	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 	//console.table(matrizCodigo)	
 	stringEncriptada = stringEncriptada.toLowerCase();
 
@@ -32,15 +39,15 @@ function encriptar(stringEncriptada){
 
 // Muestra resultados desencriptación
 function btnDesencriptar(){
-	const textoEncriptado = desencriptar(textArea.value);
-	mensaje.value = textoEncriptado;
-	textArea.value = "";
-	
+	if (validar(textArea.value)) {
+		const textoEncriptado = desencriptar(textArea.value);
+		mensaje.value = textoEncriptado;
+		textArea.value = "";
+	}	
 }
 
 // Función desencriptar
 function desencriptar(stringDesencriptada){
-	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 	//console.table(matrizCodigo)	
 	stringDesencriptada = stringDesencriptada.toLowerCase();
 
@@ -51,6 +58,23 @@ function desencriptar(stringDesencriptada){
 		}
 	}
 	return stringDesencriptada
+}
+
+//Función validación de caracteres
+function validar(textoEntrada) {
+    if(textoEntrada == "") {
+        infomsg1.style.color = "red";
+        return false;
+    }
+    //infomsg1.style.visibility = "visible";
+    //infomsg1.style.color = "#000000";
+
+    if(textoEntrada.match(excluidos)) {
+        informacion.style.color = "red";        
+        return false;
+    }
+    informacion.style.color = "#343A40";
+    return true;
 }
 
 //Función copiar mensaje
